d439cf9ca5defed6f1679bd5eeea37f32bc205e2
Alon Shaul <alon.shaul95@gmail.com>
2025-03-10 02:29:31 +0200

    Update v1.9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -102,8 +102,8 @@ const HeroSection = () => {
     <section className="relative min-h-screen flex flex-col md:flex-row items-center justify-center px-6 py-12 bg-transparent text-gray-900 dark:text-white">
       {isHebrew ? (
         <>
-          {/* עבור השפה העברית – אין שינוי */}
-          <div className="md:w-1/2" dir="rtl">
+          {/* במצב עברית – מוסיפים w-full במכשירים ניידים כדי שהטקסט יהיה ממורכז, ושומרים את ה־dir="rtl" */}
+          <div className="w-full md:w-1/2" dir="rtl">
             <motion.h1
               className="text-5xl md:text-7xl font-extrabold"
               initial={{ opacity: 0, y: -50 }}
@@ -142,7 +142,6 @@ const HeroSection = () => {
         </>
       ) : (
         <>
-          {/* עבור אנגלית ורוסית – סדר הרכיבים זהה לעברית (הטקסט מוצג באותו מיקום) */}
           <div className="md:w-1/2 text-left mt-16 md:mt-0" dir="ltr">
             <motion.h1
               className="text-5xl md:text-7xl font-extrabold"
@@ -184,8 +183,6 @@ const HeroSection = () => {
     </section>
   );
 };
-
-
 
 const SkillsSection = () => {
   const { t } = useTranslation();
